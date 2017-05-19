d864958936d85660d33eb224fc6ef4122c08099c
Sirsendu Gope <Sirsendu Gope>
2017-05-19 13:20:34 +0530

    Basic UI

---
+++ main.js
@@ -0,0 +1,20 @@
+const electron = require("electron");
+const app = electron.app;
+
+const path = require('path');
+const url = require('url');
+
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow;
+
+app.on('ready',function(){
+	mainWindow = new BrowserWindow({width:500, height:650, maximizable: false, resizable: false});
+	mainWindow.setMenu(null);
+	
+	mainWindow.loadURL(url.format({
+		pathname : path.join(__dirname, 'app/index1.html'),
+		protocol : 'file',
+		slashes : true
+	}));
+});
